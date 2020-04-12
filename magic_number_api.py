e328f86b6a38ac28c797d0d6a57bc35e4ffed062
skmisht <surenkanwalie@gmail.com>
2020-04-12 23:36:36 +0100

    updated naming convention throughout api

--- magic_number_api.py
+++ magic_number_api.py
@@ -1,23 +1,21 @@
-# api gives user multiple chances of [0,1,2] along with user's attempts
+# api gives user multiple chances of [0,1,2] along with user's attempts (3)
 import random
 magic_numbers = [random.randint(0, 9), random.randint(0, 9)]
 
 def ask_user_and_check_number():
     user_number = int(input("Enter a number between 0 and 9: "))
     if user_number in magic_numbers:
-        return "you got the number right!"
+        return "you got the Magic Number!"
     if user_number not in magic_numbers:
-        return "you did not quite get it."
+        return "you did not get the Magic Number."
 
 
 
-def run_program_x_times(chances):
+def run_program_times(chances):
     for attempt in range(chances):
-        print("This is attempt {}".format(attempt))
+        print("This is an attempt {}".format(attempt))
         message = ask_user_and_check_number()
         print(message)
 
-user_attempts = int(input("Enter the number of attempts: "))
-run_program_x_times(user_attempts)
-
-
+attempts_by_user = int(input("Enter the number of attempts: "))
+run_program_x_times(attempts_by_user)
